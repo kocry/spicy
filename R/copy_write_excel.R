4d53dc4253f1e0606c84fb4e3a75c18f7d1b7e3d
kokolet <62847529+kocry@users.noreply.github.com>
2023-01-21 04:04:57 +0100

    update copy_write_excel

--- R/copy_write_excel.R
+++ R/copy_write_excel.R
@@ -1,8 +1,8 @@
 #' Copy in the clipboard a data frame from View's RStudio to Excel
 #'
-#' @param x a data frame or matrix
-#' @param row.names either a logical value indicating whether the row names of x are to be written along with x, or a character vector of row names to be written.
-#' @param col.names either a logical value indicating whether the column names of x are to be written along with x, or a character vector of column names to be written.
+#' @param x a data frame, matrix, array (2d) or table
+#' @param row.names either a logical value (default TRUE) indicating whether the row names of x are to be written along with x, or a character vector of row names to be written.
+#' @param col.names either a logical value (default NA) indicating whether the column names of x are to be written along with x, or a character vector of column names to be written.
 #' @param ...
 #'
 #' @return a data frame in the clipboard
@@ -13,5 +13,5 @@
 #' copy_write_excel(x)
 #' }
 
-copy_write_excel <- function(x, row.names = FALSE, col.names = TRUE,...) {
+copy_write_excel <- function(x, row.names = TRUE, col.names = NA,...) {
   utils::write.table(x,"clipboard-1000000", sep="\t", row.names = row.names, col.names = col.names,...)}
